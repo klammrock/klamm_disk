d5267d1df2f5a78af3d86ea49596b12d3ff82a55
Valery Baranov <v.baranov@integer-soft.ru>
2025-06-04 01:57:29 +0300

    init spring boot

---
+++ src/main/java/name/klamm/klamm_disk/KlammDiskApplication.java
@@ -0,0 +1,13 @@
+package name.klamm.klamm_disk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlammDiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlammDiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/klamm/klamm_disk/KlammDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package name.klamm.klamm_disk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlammDiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
